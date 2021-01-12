5e2e6bda5d27d931d4b9bb2f1ddbc14b093d8789
Jhon Arias <7honarias@gmail.com>
2021-01-12 17:46:21 -0500

    [UPDATE] - incluede pillow

--- level_3/3-scrapping.py
+++ level_3/3-scrapping.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+from io import BytesIO
 import requests
-from requests import cookies
-from requests.sessions import default_headers
+import pytesseract
+from PIL import Image
 
 
-url = 'http://158.69.76.135/level2.php'
+url = 'http://158.69.76.135/level3.php'
 headwin = ("Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36")
 header = {"User-Agent": headwin, "Referer": url}
 ID = 2224
-response = requests.get(url)
+s = requests.session()
+response = s.get(url)
 text = response.text.split()
 try:
     txtId = text.index(str(ID))
@@ -21,7 +23,15 @@ if (text[txtId+3]).isdigit():
     currentVote = int(text[txtId+3])
 
 for x in range(currentVote, 29):
-    sender = requests.post(url, {'id': ID, 'holdthedoor': 'submit', 'key': key}, cookies=cookie, headers=header)
+    img_url = 'http://158.69.76.135/level3/captcha.php'
+    res_img = s.get(img_url)
+
+    original = Image.open(BytesIO(res_img.content))
+    captcha = pytesseract.image_to_string(original)
+    captcha = captcha.strip()
+    print(captcha)
+    sender = s.post(url, {'id': ID, 'holdthedoor': 'submit', 'key': key, 'captcha': captcha}, cookies=cookie, headers=header)
+
     if sender.status_code is 200 and "Hold" in sender.text:
         print("ok", end="\r", flush=True)
     else:
